f900f5dc9edb76a5a7beeada80343091588445db
Luis Martinez <luismartinezwebdev@gmail.com>
2023-08-12 18:26:29 +0200

    feat: desktop header

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,3 +1,4 @@
+import { ArrowButton } from "~/components/ArrowButton";
 import { HeroGraphic } from "~/components/HeroSection/HeroGraphic";
 import { SectionWrapper } from "~/components/SectionWrapper";
 import { StripeHeroEffect } from "~/components/StripeHeroEffect";
@@ -6,13 +7,21 @@ export function HeroSection() {
   return (
     <SectionWrapper className="xl:mt-20">
       <div className="isolate grid grid-cols-2">
-        <div>
-          <StripeHeroEffect className="z-0" />
-          <p className="mt-8 text-xl text-gray-700/90">
-            Millions of companies of all sizes—from startups to Fortune 500s—use
-            Stripe&apos;s software and APIs to accept payments, send payouts,
-            and manage their businesses online.
+        <div className="grid gap-y-[32px]">
+          <StripeHeroEffect className="z-0 h-[400px]" />
+          <p className="z-10 max-w-[45ch] text-xl text-gray-700/90">
+            Millions of businesses of all sizes—from startups to large
+            enterprises—use Stripe&apos;s software and APIs to accept payments,
+            send payouts, and manage their businesses online.
           </p>
+          <div className="flex gap-6">
+            <ArrowButton className="bg-slate-700 py-1 pl-4 pr-2 font-semibold text-white hover:bg-slate-500">
+              Start now
+            </ArrowButton>
+            <ArrowButton className=" font-semibold text-slate-700 hover:text-slate-600">
+              Contact sales
+            </ArrowButton>
+          </div>
         </div>
         <HeroGraphic className="z-10" />
       </div>
